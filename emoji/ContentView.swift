aeeade529a432c653f46ddd85ddb83dedbd33350
Renan Machado <renan.mav@hotmail.com>
2020-09-25 23:09:19 -0300

    Initial Commit

---
+++ emoji/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  emoji
+//
+//  Created by Renan Machado on 25/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ emoji/emojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  emojiApp.swift
+//  emoji
+//
+//  Created by Renan Machado on 25/09/20.
+//
+
+import SwiftUI
+
+@main
+struct emojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
